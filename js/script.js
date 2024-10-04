8db5333fad027643e3ae1362b5990755c7ee2e98
Nitesh-code-droid <nitsnuresh17@gmail.com>
2024-10-04 10:15:22 +0530

    script change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
+// document.getElementById('searchBtn').addEventListener('click', function() {
+//     let query = document.getElementById('search').value;
+//     fetchNews(query);
+// });
+
+// function fetchNews(query) {
+//     let apiKey = 'a4c8d451206c4062893eee6a85dbf200';
+//     let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
+
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         let articles = data.articles;
+//         let newsContainer = document.getElementById('newsContainer');
+//         newsContainer.innerHTML = '';
+
+//         articles.forEach(article => {
+//             let articleDiv = document.createElement('div');
+//             articleDiv.className = 'article';
+//             articleDiv.innerHTML = `
+//                 <h>${article.title}</h2>
+//                 <p>${article.description}</p>
+//                 <a href="${article.url}" target="_blank">Read more</a>
+//             `;
+//             newsContainer.appendChild(articleDiv);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error fetching news:', error);
+//     });
+// }
+
+
 document.getElementById('searchBtn').addEventListener('click', function() {
     let query = document.getElementById('search').value;
     fetchNews(query);
 });
 
 function fetchNews(query) {
-    let apiKey = 'a4c8d451206c4062893eee6a85dbf'; 
-    let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
+    let apiKey = 'pub_552922a3949cb87515af69d5d3f119f6e51fe'; 
+    let url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${encodeURIComponent(query)}`;
 
     fetch(url)
     .then(response => {
@@ -15,18 +48,27 @@ function fetchNews(query) {
         return response.json();
     })
     .then(data => {
-        if (data.articles && data.articles.length > 0) {
-            let articles = data.articles;
+        if (data.results && data.results.length > 0) {
+            let articles = data.results;
             let newsContainer = document.getElementById('newsContainer');
             newsContainer.innerHTML = '';
 
             articles.forEach(article => {
                 let articleDiv = document.createElement('div');
                 articleDiv.className = 'article';
+
+                // Limit the description to a maximum of 100 characters
+                const maxDescriptionLength = 100;
+                const truncatedDescription = article.description 
+                    ? (article.description.length > maxDescriptionLength 
+                        ? article.description.substring(0, maxDescriptionLength) + '...' 
+                        : article.description) 
+                    : 'No description available';
+
                 articleDiv.innerHTML = `
                     <h2>${article.title}</h2>
-                    <p>${article.description}</p>
-                    <a href="${article.url}" target="_blank">Read more</a>
+                    <p>${truncatedDescription}</p>
+                    <a href="${article.link}" target="_blank">Read more</a>
                 `;
                 newsContainer.appendChild(articleDiv);
             });
@@ -38,3 +80,4 @@ function fetchNews(query) {
         console.error('Error fetching news:', error);
     });
 }
+
