11de3bd28ff016766d6500b614a51bdb33c4cf1d
Nitesh-code-droid <nitsnuresh17@gmail.com>
2024-10-04 08:58:56 +0530

    api update

--- js/script.js
+++ js/script.js
@@ -4,26 +4,35 @@ document.getElementById('searchBtn').addEventListener('click', function() {
 });
 
 function fetchNews(query) {
-    let apiKey = 'a4c8d451206c4062893eee6a85dbf200';
+    let apiKey = 'a4c8d451206c4062893eee6a85dbf'; 
     let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
 
     fetch(url)
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        let articles = data.articles;
-        let newsContainer = document.getElementById('newsContainer');
-        newsContainer.innerHTML = '';
+        if (data.articles && data.articles.length > 0) {
+            let articles = data.articles;
+            let newsContainer = document.getElementById('newsContainer');
+            newsContainer.innerHTML = '';
 
-        articles.forEach(article => {
-            let articleDiv = document.createElement('div');
-            articleDiv.className = 'article';
-            articleDiv.innerHTML = `
-                <h>${article.title}</h2>
-                <p>${article.description}</p>
-                <a href="${article.url}" target="_blank">Read more</a>
-            `;
-            newsContainer.appendChild(articleDiv);
-        });
+            articles.forEach(article => {
+                let articleDiv = document.createElement('div');
+                articleDiv.className = 'article';
+                articleDiv.innerHTML = `
+                    <h2>${article.title}</h2>
+                    <p>${article.description}</p>
+                    <a href="${article.url}" target="_blank">Read more</a>
+                `;
+                newsContainer.appendChild(articleDiv);
+            });
+        } else {
+            console.error("No articles found for this query");
+        }
     })
     .catch(error => {
         console.error('Error fetching news:', error);
